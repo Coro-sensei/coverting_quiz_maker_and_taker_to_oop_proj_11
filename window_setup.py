f730d5e31ea54014abc4612e28f111c2a0352d30
Coro-sensei <acoro1719@gmail.com>
2025-05-29 23:33:54 +0800

    put the coordinates in the windows setup

--- window_setup.py
+++ window_setup.py
@@ -1,2 +1,10 @@
 # Move the window setup 
 
+def setup_window(self):
+    screen_width = self.root.winfo_screenwidth()
+    screen_height = self.root.winfo_screenheight()
+
+    coord_x = (screen_width - self.window_width) // 2
+    coord_y = (screen_height - self.window_height) // 2
+    self.root.geometry(f"{self.window_width}x{self.window}+{coord_x}+{coord_y}")
+    
