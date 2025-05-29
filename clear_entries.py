4c864ae6cc00540e8e52aa39124aeaddf086c8b7
Coro-sensei <acoro1719@gmail.com>
2025-05-30 00:19:30 +0800

    new file clear entries, clear entries

---
+++ clear_entries.py
@@ -0,0 +1,12 @@
+# Clear entries
+
+import os
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import tkinter.font as tkFont
+from quiz_maker import QuizMaker
+
+def clear_entries(self):
+        for entry in self.entries.values():
+            entry.delete(0, tk.END)
