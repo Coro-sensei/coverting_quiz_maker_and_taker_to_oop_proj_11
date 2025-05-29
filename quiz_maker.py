63a4fcfd81af6c9219874247f70ccdce8dcf2da0
Coro-sensei <acoro1719@gmail.com>
2025-05-29 23:26:06 +0800

    set the window size

--- quiz_maker.py
+++ quiz_maker.py
@@ -8,3 +8,7 @@ import tkinter.font as tkFont
 class QuizMaker:
     def __init__(self, root):
         self.root = root
+        self.root.tile("Pokemon Quiz Maker")
+        self.window_width = 1600
+        self.window_height = 800
+        self.root.resizable(False, False)
