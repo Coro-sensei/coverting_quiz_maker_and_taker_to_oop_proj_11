12376538a525ebc66286ef7c04fe846ca2f7bb03
Coro-sensei <acoro1719@gmail.com>
2025-05-29 23:22:32 +0800

    import in the class file

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,5 +1,10 @@
 # Make the quizmakerapp to oop
 
+import os
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import tkinter.font as tkFont
 class QuizMaker:
     def __init__(self, root):
         self.root = root
