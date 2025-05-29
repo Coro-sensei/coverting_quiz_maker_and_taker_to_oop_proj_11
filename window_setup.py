26a85f33ee9cf67cde2f8c9d05bafd5ebe758e34
Coro-sensei <acoro1719@gmail.com>
2025-05-29 23:48:54 +0800

    load the bg photo and new file load image

--- window_setup.py
+++ window_setup.py
@@ -1,4 +1,6 @@
 # Move the window setup 
+import tkinter as tk 
+from quiz_maker import QuizMaker
 
 def setup_window(self):
     screen_width = self.root.winfo_screenwidth()
@@ -7,4 +9,12 @@ def setup_window(self):
     coord_x = (screen_width - self.window_width) // 2
     coord_y = (screen_height - self.window_height) // 2
     self.root.geometry(f"{self.window_width}x{self.window}+{coord_x}+{coord_y}")
-    
+
+    bg_photo = self.load_image("pokeball_bg.jpeg", (self.window_wiidth, self.window_height))
+    if bg_photo:
+        tk.Label(self.root, image = bg_photo).place(x = 0, y = 0)
+        self.bg_photo = bg_photo
+        
+    else:
+        self.root.config(bg = "yellow")
+
