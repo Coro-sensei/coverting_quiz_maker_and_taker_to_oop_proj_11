f3316efc7ed9d7db5f94ccc3ff08c22edf7d7e1d
Coro-sensei <acoro1719@gmail.com>
2025-05-30 22:29:39 +0800

    main to run the program

--- quiz_taker/__pycache__/main.py
+++ quiz_taker/__pycache__/main.py
@@ -1,1 +1,12 @@
 # Main
+
+import tkinter as tk
+from user_inter import QuizMakerApp
+
+def main():
+    root = tk.Tk()
+    app = QuizMakerApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
