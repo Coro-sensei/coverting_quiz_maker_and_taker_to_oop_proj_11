46e47226c9210fc08e53725c84a37600f9403604
Coro-sensei <acoro1719@gmail.com>
2025-05-30 22:32:38 +0800

    i copy pasted the wrong code from the previous one whoops

--- quiz_taker/__pycache__/main.py
+++ quiz_taker/__pycache__/main.py
@@ -1,12 +1,2 @@
 # Main
 
-import tkinter as tk
-from user_inter import QuizMakerApp
-
-def main():
-    root = tk.Tk()
-    app = QuizMakerApp(root)
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
