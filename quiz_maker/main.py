58f973a2aa5ed1787053c9ffc57a7a64c0f5821b
Coro-sensei <acoro1719@gmail.com>
2025-05-30 22:11:54 +0800

    updated main test

--- quiz_maker/main.py
+++ quiz_maker/main.py
@@ -1,7 +1,13 @@
 # Main
 
 import tkinter as tk
-from user_inter import QuizMakerGUI
+from tkinter import messagebox
+from tkinter import font as tkFont
+from PIL import ImageTk
+from data_manager import QuizManager
+from utilities import load_image
+import os
+
 
 if __name__ == "__main__":
     root = tk.Tk()
